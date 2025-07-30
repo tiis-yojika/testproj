6d39789e491bfc7240c2dec372308440dd8b7156
tiis-yojika <yoshiharu.ojika@tiis.global>
2025-07-30 11:26:24 +0900

    Delete front directory

--- front/pages/api/favorites.js
+++
@@ -1,8 +0,0 @@
-export default async function handler(req, res) {
-  const response = await fetch(
-    "http://localhost:7071/api/favorites", // Azure FunctionsのローカルURL
-    { method: "GET" }
-  );
-  const data = await response.json();
-  res.status(200).json(data);
-}
